d42fb90f445da034f1acc172bc9e77191153ecab
zhangjianfeng <zelome@me.com>
2017-09-08 10:53:08 +0800

    Mod : Runtime Path

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -1,11 +1,11 @@
 <?php
-defined('ROOT_PATH') || define('ROOT_PATH', __DIR__ . '/');
+defined('ROOT_PATH') || define('ROOT_PATH', dirname(__DIR__) . '/');
 defined('APPLICATION_PATH') || define('APPLICATION_PATH', __DIR__ . '/');
-defined('CONFIG_PATH') || define('CONFIG_PATH', APPLICATION_PATH . '/config/');
-defined('RUNTIME_PATH') || define('RUNTIME_PATH', APPLICATION_PATH . '/runtime/');
+defined('CONFIG_PATH') || define('CONFIG_PATH', APPLICATION_PATH . 'config/');
+defined('RUNTIME_PATH') || define('RUNTIME_PATH', ROOT_PATH . 'runtime/');
 defined('KERISY_ENV') || define('KERISY_ENV', getenv('KERISY_ENV') ?: 'development');
 defined('KERISY_LANG') || define('KERISY_LANG', getenv('KERISY_LANG') ?: 'zh_cn');
-
+var_dump(RUNTIME_PATH);
 $config = [
     'applicationPath' => APPLICATION_PATH,
     'environment' => KERISY_ENV,
